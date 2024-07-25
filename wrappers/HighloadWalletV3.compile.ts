0cd35ccdd6bb08086dd41f6bc88da2f43babbf2d
Dan Volkov <hi@dvlkv.com>
2024-07-25 15:12:41 +0400

    chore: add compiled build

--- wrappers/HighloadWalletV3.compile.ts
+++ wrappers/HighloadWalletV3.compile.ts
@@ -1,6 +1,10 @@
 import { CompilerConfig } from '@ton/blueprint';
+import { writeFile } from 'fs/promises';
 
 export const compile: CompilerConfig = {
     lang: 'func',
     targets: ['contracts/highload-wallet-v3.func'],
+    postCompileHook: async (code) => {
+        await writeFile('build/highload-wallet-v3.cell', code.toBoc());
+    }
 };
